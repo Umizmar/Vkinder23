aea6b3d816454c1908588a566966d6c0b7ebcf6b
Marat Mizitov <maraentus@mail.ru>
2023-06-09 16:54:48 +0300

    some work

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import vk_api
+from vk_api.exceptions import ApiError
 
 from config import access_token
 
@@ -8,12 +9,21 @@ class Info_users:
         self.vkapi = vk_api.VkApi(token=access_token)
 
     def get_profile_info(self, user_id):
-            
-        info, = self.vkapi.method('users.get',
-        {'user_id': user_id,
-        'fields': 'city'}
-        )
-        return info
+
+        try:    
+            info, = self.vkapi.method('users.get',
+            {'user_id': user_id,
+            'fields': 'city, sex, bdate, relation'}
+            )
+        except ApiError as e:
+            info ={}
+            print(f'error {e}') 
+        result = {'name':info['first_name']+' '+info['last_name'] if 'first_name' in info and 'last_name' in info else None,
+                'sex':info.get('sex'),
+                'city':info.get('city')['title'] if info.get('city') is not None else None,
+                'bdate':info.get('bdate')}
+        return result
+        
     
 info = Info_users(access_token)
 params = info.get_profile_info(user_id=5808507)
